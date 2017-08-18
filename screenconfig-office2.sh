1092f5497d7303c1a9a54b990cebc4151405d3cc
David Fernández <david@davidfm.es>
2017-08-18 11:56:30 +0100

    Create screenconfig-office2.sh

---
+++ screenconfig-office2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1366x768 --pos 0x512 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1200 --pos 3286x232 --rotate normal --output DP1-3 --mode 1920x1200 --pos 1366x232 --rotate normal --output DP1-2 --off --output DP1-1 --off
